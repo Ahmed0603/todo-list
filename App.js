378cad4efe604882c042bfd4db2361c86be503be
Ahmed Abdulrahman <121971562+Ahmed0603@users.noreply.github.com>
2024-03-23 15:18:25 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import { Task } from "./Task";
+import { useState } from "react";
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task </button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              deleteTask={deleteTask}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
